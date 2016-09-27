cb5a4bcf2def27cde5b4b001c03b4cc9f98aedfd
Surendher <surendher@Surendhers-MacBook-Pro.local>
2016-09-27 10:38:05 +0530

    rootscope

--- example/fb-controller.js
+++ example/fb-controller.js
@@ -2,11 +2,12 @@
     'use strict';
     var module = angular.module('fbController',['common.directives.formBuilder']);
     
-    module.controller('fbController', function($scope) {
+    module.controller('fbController', function($scope, $rootScope) {
     //$scope.html = [{"id":"textbox","component":"textInput","editable":false,"index":0,"label":"Name","description":"Your name","placeholder":"Your name","options":[],"required":true,"validation":"/.*/"}];    
     $scope.goToFormViewer = function(input) {
         $scope.html = [];
         $scope.html = input;
+        $rootScope.result = input;
     };
     $scope.submit = function(form) {
         console.log(form);
